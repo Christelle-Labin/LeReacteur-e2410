1ba98b614f53383537f1e61808e04e84792a6faa
Christelle-Labin <labin.christelle@gmail.com>
2024-10-13 20:35:29 +0200

    incrementation et decrementation
    >

---
+++ incrementation-decrementation.js
@@ -0,0 +1,37 @@
+// incrementation => addition
+// decrementation => soustraction
+
+let a = 2;
+let b = 5;
+
+// addition classique : 2 + 5
+
+// si  on ajoute 1 à la variable a :
+a = a + 1;
+// console.log(a); // 3
+
+// notation abrégée pour dire qu'on ajoute 1 à a :
+a++;
+// console.log(a); // 4
+
+a = a + 7; // 11
+// console.log(a);
+
+a += 7;
+// console.log(a); // 18
+
+// decrementation : 
+b = b - 1;
+console.log(b); // 4
+
+b--;
+console.log(b); // 3
+
+b = b - 3;
+console.log(b); // 0
+
+b -= 3;
+console.log(b);
+
+
+
