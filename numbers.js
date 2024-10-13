6b4d5fc598284e858020c84d7c8e22de1d8dcc86
Christelle-Labin <labin.christelle@gmail.com>
2024-10-13 18:45:52 +0200

    formation numbers

---
+++ numbers.js
@@ -0,0 +1,27 @@
+let a = 3;
+let b = 5;
+
+// addition :
+const sum = a + b;
+console.log(sum);
+console.log("sum =>", sum);
+
+// soustraction :
+const substract = b - a;
+console.log(substract);
+console.log("substract =>", substract);
+
+
+// multiplication : 
+const product = a * b;
+console.log("product =>", product);
+
+// division :
+const result = a / b;
+console.log("result =>", result);
+
+// modulo => sert à determiner si un nombre est divisible par un autre: 
+const reste = 104566 % 7;
+console.log(reste);
+
+
