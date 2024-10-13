e565d1ee5c2abeda8b19d2b70fc4844ac0e6bb9b
Christelle-Labin <labin.christelle@gmail.com>
2024-10-13 12:02:23 +0200

    types variables

---
+++ types.js
@@ -0,0 +1,32 @@
+// types complexes : les objets et les tableaux
+
+// tyopes primitifs
+
+// chaine de caractères :
+// guillemets :
+const firstName = "Chris";
+// backticks :
+const city = `Paris`;
+// simple guillements : 
+// pour échapper un caractère, mettre un anti-slash \ => shitf + option + /
+const address = '9 rue de l\'Ouie';
+console.log(address);
+
+// Nombres (pour les valeurs numériques):
+let age = 11;
+let amount = 12.5; // attention, utiliser le point et non la virgule
+let temerature = -4;
+
+// Booléens (vrai ou faux):
+let isPresent = true;
+let isAdmin = false;
+
+// undefined => qui n'existe pas:
+let x; //undefined
+let y = undefined; 
+
+console.log(y);
+
+// null => RIEN
+let email = null;
+console.log(email);
