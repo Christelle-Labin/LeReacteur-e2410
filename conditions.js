600058a9579bdd8e72761050c67581d99422cb85
Christelle-Labin <labin.christelle@gmail.com>
2024-10-15 09:51:42 +0200

    Conditions et booléens

---
+++ conditions.js
@@ -0,0 +1,10 @@
+let city = "Londres";
+
+if (city === "Londres") {
+    // ne s'exécute que si la condition est validée
+    console.log("La ville est Londres"); 
+}
+
+console.log(city === "Londres");
+console.log(city === "Paris");
+
