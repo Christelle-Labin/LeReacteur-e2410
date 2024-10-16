02f225cc2bf9a969a892603e46f63c5f5122f37a
Christelle-Labin <labin.christelle@gmail.com>
2024-10-16 09:59:53 +0200

    Boucle for = TANT QUE

---
+++ for.js
@@ -0,0 +1,8 @@
+// dès le départ, la boucle for vous oblige à indiquer les paramètres 
+// création d'une boucle de 1 à 5
+// dans cet exemple, TANT QUE i est inférieur à 6 on entre dans la boucle
+
+for (let i = 1; i < 6; i++) {
+console.log(i);
+    
+}
