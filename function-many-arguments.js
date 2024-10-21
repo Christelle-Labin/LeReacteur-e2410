b0600d944ee6a67b3b538bdc2ac9acddf81bdd77
Christelle-Labin <labin.christelle@gmail.com>
2024-10-21 22:31:52 +0200

    function + plusieurs arguments

---
+++ function-many-arguments.js
@@ -0,0 +1,9 @@
+const presentation = (name, age) => {
+console.log("Hello !");
+console.log("How are you ?");
+console.log("My name is " + name);
+console.log("My age is " + age);
+
+};
+
+presentation("John", 24); // Attention à bien respecter l'ordre des params
