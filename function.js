df48f4a1e2ee4357a009aab044d8634cdbf187dd
Christelle-Labin <labin.christelle@gmail.com>
2024-10-21 22:27:36 +0200

    Function

---
+++ function.js
@@ -0,0 +1,22 @@
+// Déclaration 1 :
+// function sayName (params) {
+//     console.log("Hello ! My name is Chris");
+    
+// }
+
+// Déclaration 2 :
+// const sayName = function () {
+//     console.log("Hello ! My name is Chris");
+    
+// }
+
+// Déclaration 3 :
+const sayName = () => {
+        console.log("Hello ! My name is Chris");
+
+}
+
+//Appel de la fonction :
+sayName();
+sayName();
+sayName();
