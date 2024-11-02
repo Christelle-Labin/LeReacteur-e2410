8b24ce0ec4596864e42778e1c1a6fa421d8af4c2
Christelle-Labin <labin.christelle@gmail.com>
2024-11-02 23:04:27 +0100

    Object + for...in

---
+++ object-for-in.js
@@ -0,0 +1,10 @@
+const user = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 34,
+};
+
+for (const element in user) {
+    console.log(element); // // firstname, lastName, age   
+        
+    };
